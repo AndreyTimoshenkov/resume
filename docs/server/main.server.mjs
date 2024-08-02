a896769907855419ffc4f298a96d4b4f539def95
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2024-08-02 17:26:57 +0300

    gh pages deploy attempt

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4TODBW7Y.mjs";import"./chunk-UXEJAQFO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4UEXJC5F.mjs";import"./chunk-UXEJAQFO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
