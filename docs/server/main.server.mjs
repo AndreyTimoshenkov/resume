368d161daa08b111426820ea146242a04315c170
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2024-11-06 16:16:14 +0300

    fix styles a lil

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MSJBJNUL.mjs";import"./chunk-WAQSTKMC.mjs";import"./chunk-5XBVEYLF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WDHHYK4B.mjs";import"./chunk-WAQSTKMC.mjs";import"./chunk-5XBVEYLF.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
